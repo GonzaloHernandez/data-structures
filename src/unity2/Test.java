a46796cb70730ef91fbab8f8d413175687dbdb60
chalo <chalo@localhost.localdomain>
2015-11-19 08:20:41 -0500

    Folder fixed

--- src/unity2/Test.java
+++ src/unity2/Test.java
@@ -6,25 +6,49 @@
 package unity2;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
  * @author chalo
  */
 public class Test {
-    
-    public static void simpleListTest() {
-        System.out.println("-------------- Testing SimpleList ---------------");
+    public static void treeTest() {
+        System.out.println("-------------- Testing ABC Trees -----------------");
         Random r = new Random();
-        SimpleList sl = new SimpleList();
+        Tree t = new Tree();
         for (int i=0; i<10; i++) {
-            sl.add(r.nextInt(100));
+            t.add(r.nextInt(100));
         }
         System.out.print("Initial values: ");
-        sl.print();
+        t.print();
+        t.draw("Initial Values");
+        System.out.print("The tree leafs are: ");
+        t.printLeafs();
+        System.out.print("The elements of leve 2 are : ");
+        t.printLevel(2);
+        System.out.print("The path to element 2 is: ");
+        t.printPath(2);
+        System.out.println("The nearest leaf is: "+t.nearestLeaf());
+//        if (t.isComplete()) {
+//            System.out.println("This tree is Complete");
+//        }
+//        else {
+//            System.out.println("This tree isn't Complete");
+//        }
+//        t.prune();
+//        t.draw("After prunning");
+        t.balance();
+        t.balance();
+        t.draw("After balancing");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Which element do you want to remove? ");
+        int value = scan.nextInt();
+        t.remove(value);
+        t.draw("After removing the "+value+" element");
     }
     
     public static void main(String args[]) {
-        simpleListTest();
+        treeTest();
     }
 }
