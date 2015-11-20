f55e6d842f2950ecd741028ecb2f2c34792be68c
chalo <chalo@localhost.localdomain>
2015-11-19 19:06:06 -0500

    More methods

--- src/unity2/Test.java
+++ src/unity2/Test.java
@@ -15,37 +15,52 @@ import java.util.Scanner;
 public class Test {
     public static void treeTest() {
         System.out.println("-------------- Testing ABC Trees -----------------");
+
         Random r = new Random();
+        Scanner scan = new Scanner(System.in);
+
         Tree t = new Tree();
-        for (int i=0; i<10; i++) {
+        for (int i=0; i<20; i++) {
             t.add(r.nextInt(100));
         }
-        System.out.print("Initial values: ");
         t.print();
+        System.out.print("Initial values:");   
         t.draw("Initial Values");
-        System.out.print("The tree leafs are: ");
+
+        System.out.print("The tree leafs:");
         t.printLeafs();
-        System.out.print("The elements of leve 2 are : ");
-        t.printLevel(2);
-        System.out.print("The path to element 2 is: ");
-        t.printPath(2);
+
+        System.out.print("Which level do you want to see? ");
+        int level = scan.nextInt();
+        System.out.print("The elements of leve " + level + ":");
+        t.printLevel(level);
+
+        System.out.print("Which value do you want to looking path for? ");
+        int value = scan.nextInt();
+        System.out.print("Path to " + value +":");
+        t.printPath(value);
+
         System.out.println("The nearest leaf is: "+t.nearestLeaf());
-//        if (t.isComplete()) {
-//            System.out.println("This tree is Complete");
-//        }
-//        else {
-//            System.out.println("This tree isn't Complete");
-//        }
-//        t.prune();
-//        t.draw("After prunning");
+
+        if (t.isComplete()) {
+            System.out.println("This tree is Complete");
+        }
+        else {
+            System.out.println("This tree isn't Complete");
+        }
+
+        t.prune();
+        t.draw("After prunning");
+
         t.balance();
         t.balance();
         t.draw("After balancing");
-        Scanner scan = new Scanner(System.in);
+
         System.out.print("Which element do you want to remove? ");
-        int value = scan.nextInt();
+        value = scan.nextInt();
         t.remove(value);
         t.draw("After removing the "+value+" element");
+
     }
     
     public static void main(String args[]) {
